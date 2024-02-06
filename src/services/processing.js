32a2961b132b301e1cfc38c1ff25ba5c13a44b66
mikhaelHan <hancharuk270185@mail.ru>
2024-02-06 11:22:52 +0300

    feat: add os operations

--- src/services/processing.js
+++ src/services/processing.js
@@ -2,10 +2,12 @@ import { navigationUp, navigationDeep } from '../navigation/navigation.js';
 import printList from '../print-list/printList.js';
 import { readFile, addFile, renameFile, copyFile, moveFile, removeFile } from '../handling-files/handlingFiles.js';
 import Path from './Path.js';
+import printOsInformation from '../os-information/osInformation.js';
 
 async function processing(line) {
   const args = line.split(' ');
   switch (args[0].trim()) {
+
 
     case 'up':
       navigationUp();
@@ -32,25 +34,31 @@ async function processing(line) {
       if (!isfileToAdd) console.log(`Invalid input: such a file already exists "${args[1]}"...`);
       break;
 
-      case 'rn':
+    case 'rn':
       const isfileToRename = await renameFile(args[1], args[2]);
       if (!isfileToRename) console.log(`Invalid input: there is no such file "${args[1]}"...`);
       break;
 
-      case 'cp':
+    case 'cp':
       const isfileToCopy = await copyFile(args[1], args[2]);
       if (!isfileToCopy) console.log(`Invalid input: there is no such file "${args[1]}" or no such directory "${args[2]}"...`);
       break;
 
-      case 'mv':
+    case 'mv':
       const isfileToMove = await moveFile(args[1], args[2]);
       if (!isfileToMove) console.log(`Invalid input: there is no such file "${args[1]}" or no such directory "${args[2]}"...`);
       break;
 
-      case 'rm':
+    case 'rm':
       const isfileToRemove = await removeFile(args[1]);
       if (!isfileToRemove) console.log(`Invalid input: there is no such file "${args[1]}"...`);
       break;
+
+
+    case 'os':
+      await printOsInformation(args[1]);
+      break;
+
 
     default:
       console.log(`Invalid input: there is no such command "${args[0]}"...`);
